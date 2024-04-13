506e6d1d26fff3605671210ca77664cefd92e9b7
Ruslan Borysov <borysov.ruslan98@gmail.com>
2024-04-13 12:54:02 +0300

    cleared controller

--- src/main/kotlin/ua/kpi/its/lab/rest/controller/Controllers.kt
+++ src/main/kotlin/ua/kpi/its/lab/rest/controller/Controllers.kt
@@ -2,11 +2,10 @@ package ua.kpi.its.lab.rest.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import ua.kpi.its.lab.rest.dto.ExampleDto
 
 @RestController
 class ExampleController {
-    @GetMapping("/example")
-    fun example(): ExampleDto = ExampleDto("example")
+//    @GetMapping("/example")
+//    fun example(): ExampleDto = ExampleDto("example")
 }
 // Your code here
